0bf42ba6aa5a68b0ca6a18e9aa82e6325a774ef9
niazusa2012 <niaz.oracle@gmail.com>
2015-03-22 21:40:15 -0400

    Extra Credit 1,2 & 3 done

--- AgeCalculator/src/cst1201/AgeCalculator.java
+++ AgeCalculator/src/cst1201/AgeCalculator.java
@@ -10,49 +10,59 @@ import java.util.Scanner;
  */
 public class AgeCalculator {
 
-	public static void main(String[] args) {
-		//Get the current year.
-		int currentYear = LocalDate.now().getYear();
+    public static void main(String[] args) {
+        //Get the current year.
+        int currentYear = LocalDate.now().getYear();
 
-		//Ask the user for their age.
-		System.out.print("How old are you? ");
+        //Ask the user for their age.
+        System.out.print("How old are you? ");
 
-		//Create a scanner to retrieve user input.
-		Scanner scanner = new Scanner(System.in);
+        //Create a scanner to retrieve user input.
+        Scanner scanner = new Scanner(System.in);
 
-		//Retrieve user input.
-		int currentAge = scanner.nextInt();
+        //Retrieve user input.
+        int currentAge = scanner.nextInt();
 
-		//Ask the user for another year.
-		System.out.print("Enter another year: ");
-		int anotherYear = scanner.nextInt();
+        //Ask the user for another year.
+        System.out.print("Enter another year: ");
+        int anotherYear = scanner.nextInt();
 
 		//Calculate the user's age in the year retrieved in the previous step.
-		//First, we need to know how many years to add to the user's current age.
-		//This will be the other year minus the current year.
-		int yearsToAdd = anotherYear - currentYear;
+        //First, we need to know how many years to add to the user's current age.
+        //This will be the other year minus the current year.
+        int yearsToAdd = anotherYear - currentYear;
 
-		//Next, we add the result from above to the user's current age.
-		int newAge = currentAge + yearsToAdd;
+        //Next, we add the result from above to the user's current age.
+        int newAge = currentAge + yearsToAdd;
+        String word = "will be", word2 = "years";
 
-		//Finally, display the user's age in this year, but only if it's valid.
-		if (newAge >= 0)
-			System.out.println("You will be " + newAge + "-years-old in "
-				+ anotherYear + ".");
+        //Finally, display the user's age in this year, but only if it's valid.
+        if (newAge >= 0) {
+            if (newAge == 1) {
+                word2 = "year"; // extra credit 2
+                word = "were"; // extra credit 3
+            } else if (newAge < currentAge) {
+                word = "were";
+            }
+            System.out.println("You " + word + " " + newAge + " " + word2 + " old in "
+                    + anotherYear + ".");
+        } else {
+            System.out.println("Invalid age, Please try agian"); // extra credit 1
+        }
 
-		/* 
-		 EXTRA CREDIT 1: Use an else statement above to output an error message. This
-		 corresponds to the case where the user wasn't born yet.
+        /* 
+         EXTRA CREDIT 1: Use an else statement above to output an error message. This
+         corresponds to the case where the user wasn't born yet.
 		
-		 EXTRA CREDIT 2: Notice that we say "years-old" above. What if they are only 
-		 1-year-old? Fix this output for this special case above.
+         EXTRA CREDIT 2: Notice that we say "years-old" above. What if they are only 
+         1-year-old? Fix this output for this special case above.
 		
-		 EXTRA CREDIT 3: If we enter a year in the past, the output doesn't seem correct.
-		 If the year is in the past, modify the output so that it says "you were" instead
-		 of "you will."
+         EXTRA CREDIT 3: If we enter a year in the past, the output doesn't seem correct.
+         If the year is in the past, modify the output so that it says "you were" instead
+         of "you will."
 		
-		 NOTE: When submitting the extra credit, please indicate which one(s) you are
-		 attemtping.
-		 */
-	}
+         NOTE: When submitting the extra credit, please indicate which one(s) you are
+         attemtping.
+         */
+    }
 }
